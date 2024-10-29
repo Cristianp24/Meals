1229ee59de8a93dd36088225eb381c71712fa9df
Cristianp24 <113152286+Cristianp24@users.noreply.github.com>
2024-10-29 01:03:28 -0300

    Add Delete Function

--- Backend/src/Meals/mealsRoutes.js
+++ Backend/src/Meals/mealsRoutes.js
@@ -6,9 +6,9 @@ const combinedAuth = require('../Middleware/authRoutes');
 const router = Router();
 
 
-router.get("/", getAllMeals);
-router.post("/",combinedAuth, createMeal);
 router.get("/:id",getMealById);
+router.get("/", getAllMeals) ;
+router.post("/",combinedAuth, createMeal);
 router.get('/users/:userId', getUserMeals);
 router.delete("/:id", deleteMeal);
 
